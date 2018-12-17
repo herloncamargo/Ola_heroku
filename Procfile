73645b81e9e1d49d91e49a67033d8b5f04358d7a
Herlon Camargo <herloncamargo@gmail.com>
2018-12-17 15:37:01 -0200

    [ADD] Deploying Application

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn app:app
+# tag: serviço nome_do_arquivo:nome_do_serviço
--- app.py
+++ app.py
@@ -2,5 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/", methods=['GET'])
+def home():
+    return "Olá heroku, eu sou o Herlon."
+
 if __name__ == "__main__":
     app.run(debug=True)
